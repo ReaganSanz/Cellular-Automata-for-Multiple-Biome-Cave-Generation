efab2f92a68e7a94b9397dcde7ff401a6199de3a
Reagan Elizabeth Sanz <rsanz@hydra8.eecs.utk.edu>
2026-04-23 18:47:37 -0400

    Small updates to parameters, example_outputs created

--- src/biomeData.cpp
+++ src/biomeData.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 // List of biomes and their unique parameters for cave generation.
 const map<string, Biome> biome_registry = {
-    {"Forest", {"Forest", 53, 4, 3, {34, 139, 34}, {20, 20, 25}, 30, 3, 5, 20}},
+    {"Forest", {"Forest", 53, 4, 3, {34, 139, 34}, {20, 20, 25}, 30, 3, 1, 20}},
     {"Desert", {"Desert", 65, 4, 3, {237, 201, 175},{20, 20, 25},30, 3, 5, 100}},
-    {"Underwater", {"Underwater", 50, 4, 3,{0, 105, 148},{20, 52, 79},0, 3, 0,20}},
-    {"Mountain", {"Mountain", 40, 4, 2, {105, 105, 105},{20, 20, 25}, 20, 7, 5, 20}},
+    {"Underwater", {"Underwater", 50, 4, 3,{0, 105, 148},{20, 52, 79},0, 3, 1,20}},
+    {"Mountain", {"Mountain", 40, 4, 2, {105, 105, 105},{20, 20, 25}, 20, 7, 1, 20}},
 };
